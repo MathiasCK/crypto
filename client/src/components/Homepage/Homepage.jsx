12ed77f5317fdedac5abc236591a81a7f41a5320
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-11-12 11:08:53 +0100

    .

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Typography } from 'antd';
 import { CryptoCurrencies, News } from '../../components';
+import Cryptostats from './Cryptostats';
 
 const { Title } = Typography;
 
 const Homepage = () => {
   return (
     <>
+      <Cryptostats />
       <Title level={2} className='heading'>
         Top 10 Cryptocurrencies in the world
       </Title>
